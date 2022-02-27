210ab202318f47dad7d8d0b6eda6a63f102249f4
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-02-27 19:13:28 -0300

    A3.2

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -33,8 +33,8 @@ export default function HomeScreen() {
             password: values.senha,
           })
           .then(() => {
-            // router.push('/auth-page-static');
-            router.push('/auth-page-ssr');
+            router.push('/auth-page-static');
+            // router.push('/auth-page-ssr');
           })
           .catch((err) => {
             console.log(err);
--- frontend/src/services/auth/session.js
+++ frontend/src/services/auth/session.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { authService } from './authService';
+import { useRouter } from 'next/router';
 
 export function withSession(funcao) {
   return async (ctx) => {
@@ -20,5 +22,53 @@ export function withSession(funcao) {
         }
       }
     }
+  }
+}
+
+export function useSession() {
+  const [session, setSession] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    authService.getSession()
+      .then((userSession) => {
+        console.log(userSession);
+        setSession(userSession);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return {
+    data: {
+      session,
+    },
+    error,
+    loading,
+  }
+}
+
+export function withSessionHOC(Component) {
+  return function Wrapper(props) {
+    const router = useRouter();
+    const session = useSession();
+    
+    if(!session.loading && session.error) {
+      console.log('redireciona o usuário para a home');
+      router.push('/?error=401');
+    }
+
+    const modifiedProps = {
+      ...props,
+      session: session.data.session,
+    }
+    return (
+      <Component {...modifiedProps} />
+    )
   }
 }
